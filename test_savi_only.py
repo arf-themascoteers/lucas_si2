6de3b32f7f64eb123f3f4bfa0f9a7db609d929e9
Arif <arif@themascoteers.com>
2023-11-09 01:34:16 +1100

    done

---
+++ test_savi_only.py
@@ -0,0 +1,10 @@
+from evaluator import Evaluator
+import spec_utils
+
+if __name__ == "__main__":
+    column_groups = [
+        ["savi"]
+    ]
+    algorithms = ["ann_savi_only"]
+    c = Evaluator(prefix="test", folds=10, algorithms=algorithms, column_groups=column_groups)
+    c.process()
