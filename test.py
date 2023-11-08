f069fbe9e7f30eb7723be0104c9b82205c679dfe
Arif <arif@themascoteers.com>
2023-11-09 01:34:54 +1100

    done

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     column_groups = [
         spec_utils.get_wavelengths()
     ]
-    algorithms = ["ann_savi_only", "ann_savi_learned_only", "ann_savi_learned_only_bound"]
+    algorithms = ["ann_simple"]
     c = Evaluator(prefix="test", folds=10, algorithms=algorithms, column_groups=column_groups)
     c.process()
