3292c60320f4ffd2a0cc1b61576c89b6c0095eb5
Arif <arif@themascoteers.com>
2023-11-09 01:26:37 +1100

    done

--- ann_utils.py
+++ ann_utils.py
@@ -1,50 +1,12 @@
-from ann_savi_rev_0 import ANNSAVIRev0
-from ann_savi_rev_1 import ANNSAVIRev1
-from ann_savi_rev_2 import ANNSAVIRev2
-from ann_savi_rev_3 import ANNSAVIRev3
-from ann_savi_rev_4 import ANNSAVIRev4
-from ann_savi_rev_5 import ANNSAVIRev5
-from ann_savi_rev_6 import ANNSAVIRev6
-from ann_savi_rev_7 import ANNSAVIRev7
-from ann_savi_rev_8 import ANNSAVIRev8
-from ann_savi_rev_9 import ANNSAVIRev9
-from ann_savi_rev_10 import ANNSAVIRev10
-from ann_savi import ANNSAVI
-from ann_savi_bound import ANNSAVIBound
-
+from ann_simple import ANNSimple
 from ann_savi_only import ANNSAVIOnly
 from ann_savi_learned_only import ANNSAVILearnedOnly
 from ann_savi_learned_only_bound import ANNSAVILearnedOnlyBound
 
 
 def get_ann_by_name(algorithm):
-    if algorithm == "ann_savi_rev_0":
-        return ANNSAVIRev0
-    elif algorithm == "ann_savi_rev_1":
-        return ANNSAVIRev1
-    elif algorithm == "ann_savi_rev_2":
-        return ANNSAVIRev2
-    elif algorithm == "ann_savi_rev_3":
-        return ANNSAVIRev3
-    elif algorithm == "ann_savi_rev_4":
-        return ANNSAVIRev4
-    elif algorithm == "ann_savi_rev_5":
-        return ANNSAVIRev5
-    elif algorithm == "ann_savi_rev_6":
-        return ANNSAVIRev6
-    elif algorithm == "ann_savi_rev_7":
-        return ANNSAVIRev7
-    elif algorithm == "ann_savi_rev_8":
-        return ANNSAVIRev8
-    elif algorithm == "ann_savi_rev_9":
-        return ANNSAVIRev9
-    elif algorithm == "ann_savi_rev_10":
-        return ANNSAVIRev10
-    elif algorithm == "ann_savi":
-        return ANNSAVI
-    elif algorithm == "ann_savi_bound":
-        return ANNSAVIBound
-
+    if algorithm == "ann_simple":
+        return ANNSimple
     elif algorithm == "ann_savi_only":
         return ANNSAVIOnly
     elif algorithm == "ann_savi_learned_only":
